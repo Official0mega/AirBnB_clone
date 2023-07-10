add83a033219b26c0e02936b29f7ff9527dbdb30
Official0mmega <supr3m30m3ga@gmail.com>
2023-07-10 15:33:17 +0100

    Models for AirBnB Project

---
+++ models/amenity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Amenity Module for the HBNB project.
+"""
+import os
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+
+
+class Amenity(BaseModel, Base):
+    """
+    Represents an amenity data set.
+
+    Attributes:
+        __tablename__ (str): The table name for the database storage.
+        name (str): The name of the amenity.
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
