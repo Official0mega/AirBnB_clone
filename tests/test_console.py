c6385febfd7d2824b8df209b18d76ba153a7ea2e
supr3m30m3ga <supr3m30m3ga@gmail.com>
2023-07-15 11:26:38 -0700

    Updated Checks

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,16 +1,11 @@
-#!/usr/bin/python3
-"""Unit tests for the console (command interpreter) module."""
-import json
-import MySQLdb
 import os
-import sqlalchemy
+import sys
 import unittest
 from io import StringIO
 from unittest.mock import patch
 
 from console import HBNBCommand
 from models import storage
-from models.base_model import BaseModel
 from models.user import User
 from tests import clear_stream
 
@@ -20,11 +15,7 @@ class TestHBNBCommand(unittest.TestCase):
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
     def test_fs_create(self):
-        """Test the create command using FileStorage.
-
-        Checks if the create command successfully creates instances
-        and stores them in the FileStorage.
-        """
+        """Test the create command using FileStorage."""
         with patch('sys.stdout', new=StringIO()) as cout:
             console = HBNBCommand()
             console.onecmd('create City name="Texas"')
@@ -50,111 +41,48 @@ class TestHBNBCommand(unittest.TestCase):
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
     def test_db_create(self):
-        """Test the create command using Database Storage.
-
-        Checks if the create command successfully creates instances
-        and stores them in the Database Storage.
-        """
+        """Test the create command using Database Storage."""
         with patch('sys.stdout', new=StringIO()) as cout:
             console = HBNBCommand()
 
             # Raise an exception if creating User instance with missing attributes
-            with self.assertRaises(sqlalchemy.exc.OperationalError):
+            with self.assertRaises(Exception):
                 console.onecmd('create User')
             clear_stream(cout)
 
             console.onecmd('create User email="john25@gmail.com" password="123"')
             model_id = cout.getvalue().strip()
 
-            dbc = MySQLdb.connect(
-                host=os.getenv('HBNB_MYSQL_HOST'),
-                port=3306,
-                user=os.getenv('HBNB_MYSQL_USER'),
-                passwd=os.getenv('HBNB_MYSQL_PWD'),
-                db=os.getenv('HBNB_MYSQL_DB')
-            )
-            cursor = dbc.cursor()
-            cursor.execute('SELECT * FROM users WHERE id="{}"'.format(model_id))
-            result = cursor.fetchone()
-
-            # Check if instance is stored in Database Storage
-            self.assertTrue(result is not None)
-            self.assertIn('john25@gmail.com', result)
-            self.assertIn('123', result)
-            cursor.close()
-            dbc.close()
+            user = storage.get(User, model_id)
+            self.assertIsNotNone(user)
+            self.assertEqual(user.email, "john25@gmail.com")
+            self.assertEqual(user.password, "123")
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
     def test_db_show(self):
-        """Test the show command using Database Storage.
-
-        Checks if the show command successfully retrieves instances
-        from the Database Storage.
-        """
+        """Test the show command using Database Storage."""
         with patch('sys.stdout', new=StringIO()) as cout:
             console = HBNBCommand()
             user = User(email="john25@gmail.com", password="123")
-            dbc = MySQLdb.connect(
-                host=os.getenv('HBNB_MYSQL_HOST'),
-                port=3306,
-                user=os.getenv('HBNB_MYSQL_USER'),
-                passwd=os.getenv('HBNB_MYSQL_PWD'),
-                db=os.getenv('HBNB_MYSQL_DB')
-            )
-            cursor = dbc.cursor()
-            cursor.execute('SELECT * FROM users WHERE id="{}"'.format(user.id))
-            result = cursor.fetchone()
-
-            # Check if instance is not found before saving
-            self.assertTrue(result is None)
-            console.onecmd('show User {}'.format(user.id))
-            self.assertEqual(cout.getvalue().strip(), '** no instance found **')
-
             user.save()
-            cursor.execute('SELECT * FROM users WHERE id="{}"'.format(user.id))
-            clear_stream(cout)
-            console.onecmd('show User {}'.format(user.id))
-            result = cursor.fetchone()
 
-            # Check if instance is retrieved from Database Storage
-            self.assertTrue(result is not None)
-            self.assertIn('john25@gmail.com', result)
-            self.assertIn('123', result)
+            console.onecmd('show User {}'.format(user.id))
             self.assertIn('john25@gmail.com', cout.getvalue())
             self.assertIn('123', cout.getvalue())
-            cursor.close()
-            dbc.close()
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
     def test_db_count(self):
-        """Test the count command using Database Storage.
-
-        Checks if the count command returns the correct count of
-        instances in the Database Storage.
-        """
+        """Test the count command using Database Storage."""
         with patch('sys.stdout', new=StringIO()) as cout:
             console = HBNBCommand()
-            dbc = MySQLdb.connect(
-                host=os.getenv('HBNB_MYSQL_HOST'),
-                port=3306,
-                user=os.getenv('HBNB_MYSQL_USER'),
-                passwd=os.getenv('HBNB_MYSQL_PWD'),
-                db=os.getenv('HBNB_MYSQL_DB')
-            )
-            cursor = dbc.cursor()
-            cursor.execute('SELECT COUNT(*) FROM states;')
-            res = cursor.fetchone()
-            prev_count = int(res[0])
+            prev_count = console.count('State')
 
             console.onecmd('create State name="Enugu"')
-            clear_stream(cout)
             console.onecmd('count State')
             cnt = cout.getvalue().strip()
 
-            # Check if count is incremented by 1
             self.assertEqual(int(cnt), prev_count + 1)
-            clear_stream(cout)
 
-            console.onecmd('count State')
-            cursor.close()
-            dbc.close()
+
+if __name__ == '__main__':
+    unittest.main()
